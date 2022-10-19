af3d4216e6f265c93350a829a04f70eab5907560
Michelle A <michelleasuamah@Michelles-MacBook-Pro.local>
2022-10-19 14:50:02 -0400

    e6 syntax

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -1,27 +1,27 @@
-function parse_timestamp(s) {
-    //var match = s.match(/^(?:([0-9]{2,}):)?([0-5][0-9]):([0-5][0-9][.,][0-9]{0,3})/);
+const parse_timestamp = (s) => {
+    //let match = s.match(/^(?:([0-9]{2,}):)?([0-5][0-9]):([0-5][0-9][.,][0-9]{0,3})/);
     // Relaxing the timestamp format:
-    var match = s.match(/^(?:([0-9]+):)?([0-5][0-9]):([0-5][0-9](?:[.,][0-9]{0,3})?)/);
+    let match = s.match(/^(?:([0-9]+):)?([0-5][0-9]):([0-5][0-9](?:[.,][0-9]{0,3})?)/);
     if (match == null) {
         throw 'Invalid timestamp format: ' + s;
     }
-    var hours = parseInt(match[1] || "0", 10);
-    var minutes = parseInt(match[2], 10);
-    var seconds = parseFloat(match[3].replace(',', '.'));
+    let hours = parseInt(match[1] || "0", 10);
+    let minutes = parseInt(match[2], 10);
+    let seconds = parseFloat(match[3].replace(',', '.'));
     return seconds + 60 * minutes + 60 * 60 * hours;
 }
 
-function quick_and_dirty_vtt_or_srt_parser(vtt) {
-    var lines = vtt.trim().replace('\r\n', '\n').split(/[\r\n]/).map(function(line) {
+const quick_and_dirty_vtt_or_srt_parser = (vtt) => {
+    let lines = vtt.trim().replace('\r\n', '\n').split(/[\r\n]/).map(function(line) {
         return line.trim();
     });
-    var cues = [];
-    var start = null;
-    var end = null;
-    var payload = null;
-    for (var i = 0; i < lines.length; i++) {
+    let cues = [];
+    let start = null;
+    let end = null;
+    let payload = null;
+    for (let i = 0; i < lines.length; i++) {
         if (lines[i].indexOf('-->') >= 0) {
-            var splitted = lines[i].split(/[ \t]+-->[ \t]+/);
+            let splitted = lines[i].split(/[ \t]+-->[ \t]+/);
             if (splitted.length != 2) {
                 throw 'Error when splitting "-->": ' + lines[i];
             }
@@ -31,7 +31,7 @@ function quick_and_dirty_vtt_or_srt_parser(vtt) {
             end = parse_timestamp(splitted[1]);
         } else if (lines[i] == '') {
             if (start && end) {
-                var cue = new VTTCue(start, end, payload);
+                let cue = new VTTCue(start, end, payload);
                 cues.push(cue);
                 start = null;
                 end = null;
@@ -46,19 +46,20 @@ function quick_and_dirty_vtt_or_srt_parser(vtt) {
         }
     }
     if (start && end) {
-        var cue = new VTTCue(start, end, payload);
+        let cue = new VTTCue(start, end, payload);
         cues.push(cue);
     }
 
     return cues;
 }
 
-function init() {
+ 
+const init = () => {
     // http://www.html5rocks.com/en/tutorials/track/basics/
     // https://www.iandevlin.com/blog/2015/02/javascript/dynamically-adding-text-tracks-to-html5-video
-    var video = document.querySelector('video');
-    var subtitle = document.getElementById('subtitle');
-    var track = video.addTextTrack('subtitles', subtitle.dataset.label, subtitle.dataset.lang);
+    let video = document.querySelector('video');
+    let subtitle = document.getElementById('subtitle');
+    let track = video.addTextTrack('subtitles', subtitle.dataset.label, subtitle.dataset.lang);
     track.mode = "showing";
     quick_and_dirty_vtt_or_srt_parser(subtitle.innerHTML).map(function(cue) {
         track.addCue(cue);
