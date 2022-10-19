68f5f35ef799571b27d48ff52fba00ff7545f1f7
Michelle A <michelleasuamah@Michelles-MacBook-Pro.local>
2022-10-19 14:42:01 -0400

    repo cleanup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
-import myJson from '../chapters.json' assert {type: 'json'};
+import myJson from '../data/chapters.json' assert {type: 'json'};
 let summary = document.querySelector("#summary")
 
 const { chapters } = myJson
-console.log(chapters)
 summary.innerHTML = chapters.map(item => {
   return `<p id="title">${item.gist}</p><p id="headline">${item.headline}</p><p id="summ"><span id="bold-sum">Summary:</span> ${item.summary}</p>`
 }).join("")
